077580ead8ef72749ed41a684f98b4381904b231
Marc Pfister <pfister.marc-it19@it.dhbw-ravensburg.de>
2021-02-23 17:27:44 +0100

    Change the notification system from the client to publisher

--- src/io/spielo/client/Client.java
+++ src/io/spielo/client/Client.java
@@ -7,28 +7,34 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
-import io.spielo.client.events.ClientEventHandler;
+import io.spielo.client.events.ClientEventPublisher;
+import io.spielo.client.events.ClientEventSubscriber;
 import io.spielo.client.tasks.ClientReadMessageTask;
 import io.spielo.client.tasks.SendHeartbeatTask;
 import io.spielo.messages.ConnectMessage;
 import io.spielo.messages.CreateLobbyMessage;
+import io.spielo.messages.HeartbeatMessage;
+import io.spielo.messages.Message;
 import io.spielo.messages.MessageHeader;
 import io.spielo.messages.types.MessageType1;
 import io.spielo.messages.types.MessageType2;
 import io.spielo.messages.types.MessageType2Lobby;
-import io.spielo.messages.types.MessageType2Server;
 
-public class Client extends BaseClient {
+public class Client extends BaseClient implements ClientEventSubscriber{
 	
 	private final int HEARTBEAT_DELAY = 1000;
 
 	private int id;
+	private final ClientEventPublisher publisher;
 	private final Thread readThread;
 	private final ClientReadMessageTask readMessageTask;
 	private final ScheduledExecutorService executor;
 		
 	public Client() {
-		readMessageTask = new ClientReadMessageTask(this);
+		publisher = new ClientEventPublisher();
+		publisher.subscribe(this);
+		
+		readMessageTask = new ClientReadMessageTask(this, publisher);
 		
 		readThread = new Thread(readMessageTask, "Read messages thread");
 		readThread.start();
@@ -44,27 +50,23 @@ public class Client extends BaseClient {
 		send(new ConnectMessage(System.currentTimeMillis()));
 	}
 	
+	public void sendHeartbeat() {
+		send(new HeartbeatMessage(id, System.currentTimeMillis()));
+	}
+	
 	public void createLobby(final Boolean isPublic) {
 		MessageHeader header = generateHeader(MessageType1.LOBBY, MessageType2Lobby.CREATE);
 		send(new CreateLobbyMessage(header, isPublic, (byte) 0, (byte) 0, (byte) 0));
 	}
 	
-	public void subscribe(final ClientEventHandler subscriber) {
-		readMessageTask.subscribe(subscriber);
+	public void subscribe(final ClientEventSubscriber subscriber) {
+		publisher.subscribe(subscriber);
 	}
 	
-	public void unsubscribe(final ClientEventHandler subscriber) {
-		readMessageTask.unsubscribe(subscriber);
-	}
-	
-	public final void setID(final int id) {
-		this.id = id;
+	public void unsubscribe(final ClientEventSubscriber subscriber) {
+		publisher.unsubscribe(subscriber);
 	}
 
-	public final int getID() {
-		return id;
-	}
-	
 	@Override
 	public final void close() {
 		readMessageTask.shutdown();
@@ -77,6 +79,18 @@ public class Client extends BaseClient {
 	}
 	
 	public final MessageHeader generateHeader(final short receiverID, final MessageType1 type1, final MessageType2 type2) {
-		return new MessageHeader(getID(), receiverID, type1, type2, System.currentTimeMillis());
+		return new MessageHeader(id, receiverID, type1, type2, System.currentTimeMillis());
+	}
+
+	@Override
+	public void onMessageReceived(Message message) {
+		if (message instanceof ConnectMessage) {
+			id = message.getHeader().getReceiverID();
+		}
+	}
+
+	@Override
+	public void onDisconnect() {
+		
 	}
 }
--- src/io/spielo/client/tasks/ClientReadMessageTask.java
+++ src/io/spielo/client/tasks/ClientReadMessageTask.java
@@ -2,12 +2,9 @@ package io.spielo.client.tasks;
 
 import java.io.IOException;
 import java.net.SocketException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 import io.spielo.client.BaseClient;
-import io.spielo.client.events.ClientEventHandler;
+import io.spielo.client.events.ClientEventPublisher;
 import io.spielo.messages.Message;
 import io.spielo.messages.MessageFactory;
 
@@ -16,11 +13,11 @@ public class ClientReadMessageTask implements Runnable {
 	private Boolean isRunning;
 	
 	private final BaseClient client;
-	private final List<ClientEventHandler> subscribers;
+	private final ClientEventPublisher publisher;
 
-	public ClientReadMessageTask(final BaseClient client) {
+	public ClientReadMessageTask(final BaseClient client, final ClientEventPublisher publisher) {
 		this.client = client;
-		subscribers = Collections.synchronizedList(new ArrayList<>());
+		this.publisher = publisher;
 	}
 	
 	@Override
@@ -28,9 +25,7 @@ public class ClientReadMessageTask implements Runnable {
 		isRunning = true;
 		while (isRunning) {
 			try {
-				byte[] buffer = client.readByteBuffer();
-				Message message = getMessageFromBuffer(buffer);
-				notifyMessageReceivec(message);
+				receiveMessage();
 			} catch (SocketException e) {
 				if (e.getMessage().equals("Socket closed"))
 					; // Do nothing
@@ -44,33 +39,16 @@ public class ClientReadMessageTask implements Runnable {
 		}
 	}
 	
-	public void subscribe(final ClientEventHandler subscriber) {
-		subscribers.add(subscriber);
+	private final void receiveMessage() throws SocketException, IOException {
+		byte[] buffer = client.readByteBuffer();
+		
+		MessageFactory factory = new MessageFactory();
+		Message message = factory.getMessage(buffer);
+		
+		publisher.notifyMessageReceived(message);
 	}
-	
-	public void unsubscribe(final ClientEventHandler subscriber) {
-		subscribers.remove(subscriber);
-	}
-	
+
 	public void shutdown() {
 		isRunning = false;
-	}
-	
-	private Message getMessageFromBuffer(final byte[] buffer) {
-		MessageFactory factory = new MessageFactory();
-		
-		return factory.getMessage(buffer);
-	}
-	
-	private void notifyMessageReceivec(final Message message) {
-		for (ClientEventHandler subscriber : subscribers) {
-			subscriber.onMessageReceived(message);
-		}
-	}
-	
-	private void notifyDisconnect() {
-		for (ClientEventHandler subscriber : subscribers) {
-			subscriber.onDisconnect();
-		}
 	}
 }
